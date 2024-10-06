6f98dfaf2ea2d96626654d7a3017fe7929e6c727
Lunat <lukiloewe@aol.com>
2024-10-06 17:13:13 +0200

    GUI + Functionality (1st prototype)

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from PyQt5.QtCore import QSize, QTextStream
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QMainWindow, QHBoxLayout, QGridLayout, QLineEdit
+import requests
 
+from PyQt5.QtCore import QSize
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QMainWindow, QLineEdit, QVBoxLayout, QLabel
+
+API_Key = '9b28c7a3bcea595744fc43c16882304f'
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -8,23 +11,44 @@ class MainWindow(QMainWindow):
 
         self.setWindowTitle("Py Weather App")
 
-        layout = QGridLayout()
+        layout = QVBoxLayout()
 
-        zip_input = QLineEdit()
-        zip_input.setMaxLength(6)
-        zip_input.setPlaceholderText("Enter Zip Code")
-        button_send = QPushButton("Send")
-        button_clear = QPushButton("New Zip")
+        self.zip_input = QLineEdit(self)
+        self.zip_input.setPlaceholderText("Enter city or zip code")
+        self.button_send = QPushButton("Send")
+        self.button_send.clicked.connect(self.get_wetter)
+        self.button_clear = QPushButton("New city / zip")
+        self.button_clear.clicked.connect(self.clear_zip)
+        self.label_result = QLabel('Weather Data is shown here...')
 
-        layout.addWidget(zip_input, 0, 0)
-        layout.addWidget(button_send, 0, 1)
-        layout.addWidget(button_clear, 3, 3)
+        layout.addWidget(self.zip_input)
+        layout.addWidget(self.button_send)
+        layout.addWidget(self.button_clear)
+        layout.addWidget(self.label_result)
 
 
-        self.setFixedSize(QSize(800, 500))
+        self.setFixedSize(QSize(250, 250))
         widget = QWidget()
         widget.setLayout(layout)
         self.setCentralWidget(widget)
+
+    def get_wetter(self):
+        city = self.zip_input.text()
+        if city:
+            url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&APPID={API_Key}&units=metric'
+            response = requests.get(url)
+            if response.status_code == 200:
+                data = response.json()
+                temperature = data['main']['temp']
+                weather_description = data['weather'][0]['description']
+                self.label_result.setText(f'Temperature: {temperature}°C\nDescription: {weather_description}')
+            else:
+                self.label_result.setText(f'City or zip code not found')
+
+    def clear_zip(self):
+        self.zip_input.setText('')
+        self.label_result.setText('Weather Data is shown here...')
+
 
 app = QApplication([])
 
